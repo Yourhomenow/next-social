2625a6714acb6c548038685290a975eb96f24b47
Yourhomenow <123375532+Yourhomenow@users.noreply.github.com>
2026-06-23 15:35:29 +0700

    Main Ad

--- src/app/components/Ad.tsx
+++ src/app/components/Ad.tsx
@@ -1,7 +1,51 @@
+import Image from "next/image"
+
 const Ad = ({size}:{size:"sm"|"md"|"lg"}) => {
   return (
     <div className="p-4 bg-white rounded-lg shadow-md text-sm">
-      Ad
+      {/* TOP */}
+      <div className="flex items-center justify-between text-gray-500 font-medium">
+        <span>
+          Sponsored Ad
+        </span>
+        <Image 
+          src="/more.png"
+          alt=""
+          width={16}
+          height={16}
+        />
+      </div>
+      {/* BOTTOM */}
+      <div className={`flex flex-col mt-4 ${size === "sm" ? "gap-2" : "gap-4"}`}>
+        <div className={`relative w-full ${size === "sm" ? "h-24" : size === "md" ? "h-36" : "h-48"}`}>
+          <Image 
+            src="https://images.unsplash.com/photo-1781088154901-36177e307015?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+            fill
+            className="rounded-lg object-cover"
+          />
+        </div>
+        <div className="flex items-center gap-4">
+          <Image 
+            src="https://images.unsplash.com/photo-1781088154901-36177e307015?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+            width={24}
+            height={24}
+            className="rounded-full w-6 h-6 object-cover"
+          />
+          <span className="text-blue-500 font-medium">BigChef Lounge</span>
+        </div>
+        <p className={size==="sm" ? "text-xs" : "text-sm"}>
+          {size === "sm" 
+            ? "Lorem ipsum dolor sit amet consectetur adipisicing elit." 
+            : size === "md" 
+            ? "Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit." 
+            : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit." }
+        </p>
+        <button className="bg-gray-200 text-gray-500 p-2 text-xs rounded-lg">
+          Learn more
+        </button>
+      </div>
     </div>
   )
 }
