9f6a94a69acee749b6b16f89a2896d70e5dac1c0
Yourhomenow <123375532+Yourhomenow@users.noreply.github.com>
2026-06-24 13:03:22 +0700

    user media

--- src/app/components/UserMediaCard.tsx
+++ src/app/components/UserMediaCard.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image"
 import Link from "next/link"
 
 const UserMediaCard = ({userId} : {userId:string}) => {
@@ -9,6 +10,49 @@ const UserMediaCard = ({userId} : {userId:string}) => {
         <Link href="/" className="text-blue-500 text-xs"> 
           See all
         </Link>
+      </div>
+      {/* BOTTOM */}
+      <div className="flex gap-4 justify-between flex-wrap">
+        <div className="relative w-1/5 h-24">
+          <Image 
+            src="https://images.unsplash.com/photo-1781185844992-a89d38f865e5?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+            alt="" 
+            fill 
+            className="object-cover rounded-md"
+          />
+        </div>
+        <div className="relative w-1/5 h-24">
+          <Image 
+            src="https://images.unsplash.com/photo-1781185844992-a89d38f865e5?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+            alt="" 
+            fill 
+            className="object-cover rounded-md"
+          />
+        </div>
+        <div className="relative w-1/5 h-24">
+          <Image 
+            src="https://images.unsplash.com/photo-1781185844992-a89d38f865e5?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+            alt="" 
+            fill 
+            className="object-cover rounded-md"
+          />
+        </div>
+        <div className="relative w-1/5 h-24">
+          <Image 
+            src="https://images.unsplash.com/photo-1781185844992-a89d38f865e5?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+            alt="" 
+            fill 
+            className="object-cover rounded-md"
+          />
+        </div>
+        <div className="relative w-1/5 h-24">
+          <Image 
+            src="https://images.unsplash.com/photo-1781185844992-a89d38f865e5?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+            alt="" 
+            fill 
+            className="object-cover rounded-md"
+          />
+        </div>
       </div>
     </div>
   )
