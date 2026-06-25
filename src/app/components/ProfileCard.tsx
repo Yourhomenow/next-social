e2285a778abb052149c9fb535adbe50250ad2427
Yourhomenow <123375532+Yourhomenow@users.noreply.github.com>
2026-06-25 13:18:21 +0700

    left menu

---
+++ src/app/components/ProfileCard.tsx
@@ -0,0 +1,59 @@
+import Image from "next/image"
+
+const ProfileCard = () => {
+  return (
+    <div className="p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-6">
+      <div className="h-20 relative">
+        <Image 
+          src="https://images.unsplash.com/photo-1780468125977-02e6f59617f8?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+          alt="" 
+          fill 
+          className="rounded-md object-cover"
+        />
+        <Image 
+          src="https://images.unsplash.com/photo-1780211662775-fd0ab461fe0f?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+          alt=""
+          width={48}
+          height={48} 
+          className="rounded-full object-cover w-12 h-12 absolute left-0 right-0 m-auto -bottom-6 ring-1 ring-white z-10"
+        />
+      </div>
+      <div className="h-20 flex flex-col gap-2 items-center">
+        <span className="font-semibold">Miguel Mikky</span>
+        <div className="flex items-center gap-4">
+          <div className="flex">
+            <Image 
+              src="https://images.unsplash.com/photo-1780211662775-fd0ab461fe0f?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+              alt=""
+              width={12}
+              height={12} 
+              className="rounded-full object-cover w-3 h-3"
+            />
+            <Image 
+              src="https://images.unsplash.com/photo-1780211662775-fd0ab461fe0f?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+              alt=""
+              width={12}
+              height={12} 
+              className="rounded-full object-cover w-3 h-3"
+            />
+            <Image 
+              src="https://images.unsplash.com/photo-1780211662775-fd0ab461fe0f?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+              alt=""
+              width={12}
+              height={12} 
+              className="rounded-full object-cover w-3 h-3"
+            />
+          </div>
+          <span className="text-xs text-gray-500">
+            500 followers
+          </span>
+        </div>
+        <button className="bg-blue-500 text-white text-xs p-2 rounded-md">
+          My Profile
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default ProfileCard
